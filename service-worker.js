ff985447f5f887197b56ede54ea3bee670cd1066
Pol Rovira <proviraguilar@gmail.com>
2026-07-26 22:52:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,24 @@
-const CACHE_NAME = 'programeta-shell-v2';
-const APP_SHELL = [
+const CACHE_NAME = 'programeta-v2-shell-1';
+const STATIC_ASSETS = [
   './',
   './index.html',
   './css/style.css',
   './js/app.firebase.js',
   './js/firebase.js',
   './js/utils.js',
-  './assets/logo-wordmark.svg',
-  './assets/logo-icon.svg',
+  './manifest.webmanifest',
   './assets/logo-icon.png',
-  './assets/icon-192.png',
-  './assets/icon-512.png',
-  './manifest.webmanifest'
+  './assets/logo-icon2.png',
+  './assets/logo-tipo-umbra.png',
+  './assets/logo-tipo-yellow.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', event => {
@@ -28,29 +30,22 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
-
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
 
   event.respondWith(
     caches.match(event.request).then(cached => {
-      const network = fetch(event.request).then(response => {
-        if (response.ok) {
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()));
-        }
-        return response;
-      });
+      const network = fetch(event.request)
+        .then(response => {
+          if (response.ok) {
+            const clone = response.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+          }
+          return response;
+        })
+        .catch(() => cached);
+
       return cached || network;
     })
   );
